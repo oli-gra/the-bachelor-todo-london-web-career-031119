03b3bde1e598f9d9c8868478a413e0151898ef3d
Oliver <oliver.grant@gmail.com>
2019-01-29 18:28:02 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,13 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |seasons,contestants|
+    contestants.each do |stats|
+      if stats["hometown"] == hometown
+        return stats["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
