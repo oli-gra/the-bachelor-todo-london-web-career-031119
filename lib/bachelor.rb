ebc0c7f772b55e35c1e1a38b8fcffbbedef2fb45
Oliver <oliver.grant@gmail.com>
2019-01-29 18:11:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -16,6 +16,7 @@ def get_first_name_of_season_winner(data, season)
   data.each do |seasons,contestants|
     if seasons == season
       contestants.each do |stats|
+        binding.pry
         if stats["status"] == "winner"
           return stats["name"]
         end
