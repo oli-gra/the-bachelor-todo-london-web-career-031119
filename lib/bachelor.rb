85c5a837271b066245b28479a5ba6318ce955a08
Oliver <oliver.grant@gmail.com>
2019-01-29 18:16:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -17,7 +17,7 @@ def get_first_name_of_season_winner(data, season)
     if seasons == season
       contestants.each do |stats|
         if stats["status"] == "Winner"
-          return stats["name"]
+          return stats["name"].split(" ")[0]
         end
       end
     end
