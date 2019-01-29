2e8f3b73922e43138e729543958d86ed3516af76
Oliver <oliver.grant@gmail.com>
2019-01-29 18:37:10 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -59,6 +59,7 @@ end
 def get_average_age_for_season(data, season)
   age_of = 0.0
   number_of = 0
+  average_age = 0
   data.each do |seasons,contestants|
     if seasons == season
       contestants.each do |stats|
